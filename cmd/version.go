ebe07d48589d6437d2b81749a9e350d0f62dae05
ocean <429472406@qq.com>
2021-04-15 17:59:19 +0800

    update

--- cmd/version.go
+++ cmd/version.go
@@ -8,10 +8,12 @@ import (
 func Version() *cobra.Command {
 	versionCmd := &cobra.Command{
 		Use:   "version",
-		Short: "Print the version number of Jobor",
+		Short: "Print the version of Jobor",
 		Long:  `This is Mars plan jobor cron`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("jobor version is v1.0")
+			fmt.Printf("Version   : %s\n", "v1.0.0")
+			fmt.Printf("Commit    : %s\n", "")
+			fmt.Printf("BuildDate : %s\n", "2021.4.3")
 		},
 	}
 	return versionCmd
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module jobor
 go 1.15
 
 require (
-	github.com/go-redis/redis/v7 v7.4.0
+	github.com/go-redis/redis/v8 v8.8.0
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/onsi/ginkgo v1.15.2 // indirect
 	github.com/onsi/gomega v1.11.0 // indirect
 	github.com/spf13/cobra v1.1.3
