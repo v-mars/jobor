301ce928222c26c6fcf3efb5421269c0f0757caa
ocean <429472406@qq.com>
2022-04-19 18:36:45 +0800

    fix long time task grpc goway error

--- internal/proto/index.go
+++ internal/proto/index.go
@@ -1,11 +1,11 @@
 package proto
 
 import (
-	"jobor/internal/proto/pb"
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
+	"jobor/internal/proto/pb"
 	"time"
 )
 
@@ -26,7 +26,7 @@ var Kaep = keepalive.EnforcementPolicy{
 
 var Kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAge:      0 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
 	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
 	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
